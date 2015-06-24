f8816cdcc30c6db9b00a2db0eee0296210dd8efd
Shota.Sakamoto <Shota.Sakamoto@access-company.com>
2015-06-24 15:33:59 +0900

    initiali commit.

---
+++ VoiceConverterSampleApp/VoiceConverterSampleApp.Windows/MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// 空白ページのアイテム テンプレートについては、http://go.microsoft.com/fwlink/?LinkId=234238 を参照してください
+
+namespace VoiceConverterSampleApp
+{
+    /// <summary>
+    /// Frame 内へナビゲートするために利用する空欄ページ。
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
---
+++ VoiceConverterSampleApp/VoiceConverterSampleApp.WindowsPhone/MainPage.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// 空白ページのアイテム テンプレートについては、http://go.microsoft.com/fwlink/?LinkId=234238 を参照してください
+
+namespace VoiceConverterSampleApp
+{
+    /// <summary>
+    /// Frame 内へナビゲートするために利用する空欄ページ。
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            this.NavigationCacheMode = NavigationCacheMode.Required;
+        }
+
+        /// <summary>
+        /// このページがフレームに表示されるときに呼び出されます。
+        /// </summary>
+        /// <param name="e">このページにどのように到達したかを説明するイベント データ。
+        /// このプロパティは、通常、ページを構成するために使用します。</param>
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            // TODO: ここに表示するページを準備します。
+
+            // TODO: アプリケーションに複数のページが含まれている場合は、次のイベントの
+            // 登録によりハードウェアの戻るボタンを処理していることを確認してください:
+            // Windows.Phone.UI.Input.HardwareButtons.BackPressed イベント。
+            // 一部のテンプレートで指定された NavigationHelper を使用している場合は、
+            // このイベントが自動的に処理されます。
+        }
+    }
+}
